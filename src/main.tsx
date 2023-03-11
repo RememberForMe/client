e7bc33b9d7a6176fc60182b0329f32ead2486294
VietAnh188 <anhkun123456@gmail.com>
2023-03-11 11:46:27 +0700

    setup apollo graphql

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
+import {
+    ApolloProvider,
+    InMemoryCache,
+    ApolloClient,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:3000/graphql/'
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <ApolloProvider client={client}>
+            <RouterProvider router={router} />
+        </ApolloProvider>
     </React.StrictMode>,
 );
